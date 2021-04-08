892ecef58595134addb85b2637816daf8c93c1a4
Riccardo Brero <riki.brero@gmail.com>
2021-04-08 23:04:47 +0200

    Upgrade

--- firebloc/lib/firebloc.dart
+++ firebloc/lib/firebloc.dart
@@ -1,16 +1,13 @@
 library firebloc;
 
 export 'src/blocs/main_bloc.dart';
-export 'src/blocs/main_state.dart';
 export 'src/blocs/main_event.dart';
-
-export 'src/repositories/main_repository.dart';
+export 'src/blocs/main_state.dart';
 
 export 'src/models/main_model.dart';
 
+export 'src/repositories/base_repository.dart';
+export 'src/repositories/main_repository.dart';
+
 export 'src/utilities/firebloc_utilities.dart';
 export 'src/utilities/firebloc_widgets.dart';
-
-//
-
-export 'src/repositories/base_repository.dart';
